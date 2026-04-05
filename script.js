3c472aa309bbe6dec2f0eb1d0cef6f183d782ede
Diego ramon <126127427+Diegoramonn@users.noreply.github.com>
2026-04-05 20:27:19 -0300

    atu

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ function verProjeto(){
 }
 
 console.log("Portfólio carregado")
+
+window.onload = function(){
+    let cards = document.querySelectorAll("card")
+    cards.forEach(( card,i)=>{
+
+        card.sytyle.opacity = 0
+
+        setTimeout(()=>{
+            card.style.transition="0.5s"
+            card.style.opacity = 1
+        }, 1*200)
+    })
+}
